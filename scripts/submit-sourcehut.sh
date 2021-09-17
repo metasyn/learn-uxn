da8d03ecf00406103ba774122bea8336526e1f9a
Xander <alexx.johnson@gmail.com>
2021-09-17 16:22:01 +0000

    update script.

--- scripts/submit-sourcehut.sh
+++ scripts/submit-sourcehut.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
+BASE=$(dirname "$0")
+
+pushd ${BASE}/..
+
+if [[ -f ".env" ]]; then
+    source .env
+fi;
+
 if [[ -z ${TOKEN} ]]; then
     echo 'TOKEN must be defined';
     exit 1;
 fi
 
-MANIFEST=$(cat .build.yml);
+if [[ -f ".build.yml" ]]; then
+    MANIFEST=$(cat .build.yml);
+else
+    echo ".build.yml file doesn't exist."
+    exit 1;
+fi
 
 curl \
     --request POST \
