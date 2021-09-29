a6cceae47d4272c35c1d0eeb164add02b19794bb
Xander Johnson <xander@metasyn.pw>
2021-09-29 00:40:54 -0700

    add new/better integration tests.

--- cypress/integration/e2e_spec.js
+++ cypress/integration/e2e_spec.js
@@ -4,7 +4,7 @@ Cypress.on('window:before:load', (win) => {
   windowErrorSpy = cy.spy(win.console, 'error');
 });
 
-const DELAY = 1000;
+const DELAY = 1500;
 
 describe('learn-uxn integration tests', () => {
   afterEach(() => {
@@ -18,7 +18,7 @@ describe('learn-uxn integration tests', () => {
     cy.visit('http://localhost:8000/site');
 
     // wait a second for things to load
-    cy.wait(1000);
+    cy.wait(DELAY);
 
     // iframe should load for emu
     cy.get('#uxnemu-iframe')
@@ -31,14 +31,25 @@ describe('learn-uxn integration tests', () => {
     cy.get('#editor').should('exist');
   });
 
-  it('can load a rom, and assemble', () => {
+  it('can load all roms, and assemble', () => {
     cy.visit('http://localhost:8000/site');
     // wait a second for things to load
-    cy.wait(1000);
+    cy.wait(DELAY);
 
-    // force true to make get around the hover visibility
-    cy.get('#roms').find('a').first().click({ force: true });
-    cy.wait(1000);
-    cy.get('#assemble').click();
+    // I should be able to calculate this from a selector
+    // but cy didn't seem to be returning the length; hard coded
+    // the rom count for now
+    for (let i = 1; i < 21; i += 1) {
+      cy.log(i);
+      // force true to make get around the hover visibility
+      cy.get(`#roms-list > a:nth-child(${i})`).click({ force: true });
+      cy.wait(DELAY);
+
+      // on the first load, we have a failed rom load
+      // but there should not be more than one
+      cy.get('#console')
+        .get('span:contains(Failed to open rom)')
+        .should('have.length', 1);
+    }
   });
 });
